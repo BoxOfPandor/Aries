4040fcb8d0bac684b887ee6b03051cb277f50118
Thibault Pouch <89460756+BoxOfPandor@users.noreply.github.com>
2024-03-21 12:07:30 +0100

    [+] add a file for the roll of dice

---
+++ dice.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2024
+** Aries
+** File description:
+** dice.c
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int role_d6(void)
+{
+    return (rand() % 6 + 1);
+}
+
+int role_stat(void)
+{
+    int d1 = role_d6();
+    int d2 = role_d6();
+    int d3 = role_d6();
+    int d4 = role_d6();
+    int d5 = role_d6();
+    int min = d1;
+
+    if (d2 < min)
+        min = d2;
+    if (d3 < min)
+        min = d3;
+    if (d4 < min)
+        min = d4;
+    if (d5 < min)
+        min = d4;
+    return (d1 + d2 + d3 + d4 - min);
+}
